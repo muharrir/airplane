148cf6cb82ad96f2c6c506f32774c185ffbe03a3
Muharrir <muharrir.@MacBook-Air.local>
2022-07-21 12:16:41 +0700

    first commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//NOTE: DEFAULT MARGIN & RADIUS
+double defaultMargin = 24.0;
+double defaultRadius = 17.0;
+
+//NOTE: COLOR
+Color kPrimaryColor = const Color(0xff5C40CC);
+Color kBlackColor = const Color(0xff1F1449);
+Color kWhiteColor = const Color(0xffFFFFFF);
+Color kGreyColor = const Color(0xff9698A9);
+Color kGreenColor = const Color(0xff0EC3AE);
+Color kRedColor = const Color(0xffEB70A5);
+Color kBackgroundColor = const Color(0xffFAFAFA);
+Color kInactiveColor = const Color(0xffDBD7EC);
+
+//NOTE: TEXTSTYLE
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: kWhiteColor,
+);
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: kGreyColor,
+);
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: kGreenColor,
+);
+TextStyle redTextStyle = GoogleFonts.poppins(
+  color: kRedColor,
+);
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: kPrimaryColor,
+);
+
+//NOTE: FONTWEIGHT
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../../shared/theme.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              margin: const EdgeInsets.only(bottom: 50),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/icon_plane.png'),
+                ),
+              ),
+            ),
+            Text(
+              'AIRPLANE',
+              style: whiteTextStyle.copyWith(
+                fontSize: 32,
+                fontWeight: medium,
+                letterSpacing: 10,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

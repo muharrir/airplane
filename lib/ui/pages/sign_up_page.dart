924ba53398e12e4800ee46179557a57bc1005944
Muharrir <muharrir.@MacBook-Air.local>
2022-07-25 20:48:01 +0700

    added bonus page

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -6,6 +6,226 @@ class SignUpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget title() {
+      return Container(
+        margin: const EdgeInsets.only(top: 30),
+        child: Text(
+          'Join us and get\nyour next journey',
+          style: blackTextStyle.copyWith(
+            fontSize: 24,
+            fontWeight: semiBold,
+          ),
+        ),
+      );
+    }
+
+    Widget inputSection() {
+      Widget nameInput() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Full Name',
+                style: blackTextStyle,
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(18),
+                  hintText: 'Your Full Name',
+                  hintStyle: greyTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(color: kPrimaryColor),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget emailInput() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Email Address',
+                style: blackTextStyle,
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(18),
+                  hintText: 'Your Email Address',
+                  hintStyle: greyTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(color: kPrimaryColor),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget passwordInput() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Password',
+                style: blackTextStyle,
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                obscureText: true,
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(18),
+                  hintText: 'Your Password',
+                  hintStyle: greyTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(color: kPrimaryColor),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget hobbyInput() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hobby',
+                style: blackTextStyle,
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(18),
+                  hintText: 'Your Hobby',
+                  hintStyle: greyTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(color: kPrimaryColor),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget submitButton() {
+        return SizedBox(
+          width: double.infinity,
+          height: 55,
+          child: TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/bonus');
+            },
+            style: TextButton.styleFrom(
+              backgroundColor: kPrimaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(defaultRadius),
+              ),
+            ),
+            child: Text(
+              'Get Started',
+              style: whiteTextStyle.copyWith(
+                fontSize: 18,
+                fontWeight: medium,
+              ),
+            ),
+          ),
+        );
+      }
+
+      return Container(
+        margin: const EdgeInsets.only(top: 30),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 30,
+        ),
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.circular(defaultRadius),
+        ),
+        child: Column(
+          children: [
+            nameInput(),
+            emailInput(),
+            passwordInput(),
+            hobbyInput(),
+            submitButton(),
+          ],
+        ),
+      );
+    }
+
+    Widget tacButton() {
+      return Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.only(
+          top: 50,
+          bottom: 73,
+        ),
+        child: Text(
+          'Terms and Conditions',
+          style: greyTextStyle.copyWith(
+            fontSize: 16,
+            fontWeight: light,
+            decoration: TextDecoration.underline,
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: SafeArea(
@@ -16,224 +236,6 @@ class SignUpPage extends StatelessWidget {
             inputSection(),
             tacButton(),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget title() {
-    return Container(
-      margin: const EdgeInsets.only(top: 30),
-      child: Text(
-        'Join us and get\nyour next journey',
-        style: blackTextStyle.copyWith(
-          fontSize: 24,
-          fontWeight: semiBold,
-        ),
-      ),
-    );
-  }
-
-  Widget inputSection() {
-    Widget nameInput() {
-      return Container(
-        margin: const EdgeInsets.only(bottom: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Full Name',
-              style: blackTextStyle,
-            ),
-            const SizedBox(
-              height: 6,
-            ),
-            TextFormField(
-              cursorColor: kBlackColor,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.all(18),
-                hintText: 'Your Full Name',
-                hintStyle: greyTextStyle.copyWith(
-                  fontSize: 16,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(defaultRadius),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(defaultRadius),
-                  borderSide: BorderSide(color: kPrimaryColor),
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    }
-
-    Widget emailInput() {
-      return Container(
-        margin: const EdgeInsets.only(bottom: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Email Address',
-              style: blackTextStyle,
-            ),
-            const SizedBox(
-              height: 6,
-            ),
-            TextFormField(
-              cursorColor: kBlackColor,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.all(18),
-                hintText: 'Your Email Address',
-                hintStyle: greyTextStyle.copyWith(
-                  fontSize: 16,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(defaultRadius),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(defaultRadius),
-                  borderSide: BorderSide(color: kPrimaryColor),
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    }
-
-    Widget passwordInput() {
-      return Container(
-        margin: const EdgeInsets.only(bottom: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Password',
-              style: blackTextStyle,
-            ),
-            const SizedBox(
-              height: 6,
-            ),
-            TextFormField(
-              obscureText: true,
-              cursorColor: kBlackColor,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.all(18),
-                hintText: 'Your Password',
-                hintStyle: greyTextStyle.copyWith(
-                  fontSize: 16,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(defaultRadius),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(defaultRadius),
-                  borderSide: BorderSide(color: kPrimaryColor),
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    }
-
-    Widget hobbyInput() {
-      return Container(
-        margin: const EdgeInsets.only(bottom: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Hobby',
-              style: blackTextStyle,
-            ),
-            const SizedBox(
-              height: 6,
-            ),
-            TextFormField(
-              cursorColor: kBlackColor,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.all(18),
-                hintText: 'Your Hobby',
-                hintStyle: greyTextStyle.copyWith(
-                  fontSize: 16,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(defaultRadius),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(defaultRadius),
-                  borderSide: BorderSide(color: kPrimaryColor),
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    }
-
-    Widget submitButton() {
-      return SizedBox(
-        width: double.infinity,
-        height: 55,
-        child: TextButton(
-          onPressed: () {},
-          style: TextButton.styleFrom(
-            backgroundColor: kPrimaryColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(defaultRadius),
-            ),
-          ),
-          child: Text(
-            'Get Started',
-            style: whiteTextStyle.copyWith(
-              fontSize: 18,
-              fontWeight: medium,
-            ),
-          ),
-        ),
-      );
-    }
-
-    return Container(
-      margin: const EdgeInsets.only(top: 30),
-      padding: const EdgeInsets.symmetric(
-        horizontal: 20,
-        vertical: 30,
-      ),
-      decoration: BoxDecoration(
-        color: kWhiteColor,
-        borderRadius: BorderRadius.circular(defaultRadius),
-      ),
-      child: Column(
-        children: [
-          nameInput(),
-          emailInput(),
-          passwordInput(),
-          hobbyInput(),
-          submitButton(),
-        ],
-      ),
-    );
-  }
-
-  Widget tacButton() {
-    return Container(
-      alignment: Alignment.center,
-      margin: const EdgeInsets.only(
-        top: 50,
-        bottom: 73,
-      ),
-      child: Text(
-        'Terms and Conditions',
-        style: greyTextStyle.copyWith(
-          fontSize: 16,
-          fontWeight: light,
-          decoration: TextDecoration.underline,
         ),
       ),
     );
